ceabad29e6524540069d80bf1753fcb1d8d67e65
Daniel Robbins <drobbins@funtoo.org>
2020-03-12 14:34:05 -0600

    Adding a basic HTTP page fetcher.

--- pkgtools/fetch.py
+++ pkgtools/fetch.py
@@ -21,4 +21,14 @@ async def get_url_from_redirect(hub, url):
 		raise FetchError("Couldn't get URL %s -- %s" % (url, repr(e)))
 	raise FetchError("URL %s doesn't appear to redirect" % url)
 
+async def get_page(hub, url):
+	http_client = httpclient.AsyncHTTPClient()
+	try:
+		req = HTTPRequest(url=url, follow_redirects=False)
+		response = await http_client.fetch(req)
+		return response.body.decode()
+	except Exception as e:
+		raise FetchError("Couldn't get URL %s -- %s" % (url, repr(e)))
+
+
 # vim: ts=4 sw=4 noet
