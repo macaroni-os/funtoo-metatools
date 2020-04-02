e8cc6abc1e7eba0a5e31ec711e93e0dd6880d974
Daniel Robbins <drobbins@funtoo.org>
2020-04-01 19:28:35 -0600

    Add YAML mass auto-generation.

--- pkgtools/pkgtools/autogen.py
+++ pkgtools/pkgtools/autogen.py
@@ -4,18 +4,25 @@ import subprocess
 import os
 import logging
 import traceback
+from yaml import safe_load
+
+def generate_manifests(hub):
+	for manifest_file, manifest_lines in hub.MANIFEST_LINES.items():
+		manifest_lines = sorted(list(manifest_lines))
+		with open(manifest_file, "w") as myf:
+			pos = 0
+			while pos < len(manifest_lines):
+				if pos != 0:
+					myf.write('\n')
+				myf.write(manifest_lines[pos])
+				pos += 1
+		logging.debug(f"Manifest {manifest_file} generated.")
 
 
-async def start(hub, start_path=None, out_path=None):
-
+async def generate_individual_autogens(hub):
 	"""
-	This method will start the auto-generation of packages in an ebuild repository.
+	This method finds individual autogen.py files in the current repository path and runs them all.
 	"""
-
-	hub.pkgtools.repository.set_context(
-		start_path if start_path is not None else hub.OPT.pkgtools.start_path,
-		out_path=out_path if out_path is not None else hub.OPT.pkgtools.out_path, name=hub.OPT.pkgtools.name)
-
 	s, o = subprocess.getstatusoutput("find %s -iname autogen.py 2>&1" % hub.OPT.pkgtools['start_path'])
 	files = o.split('\n')
 	for file in files:
@@ -46,15 +53,70 @@ async def start(hub, start_path=None, out_path=None):
 		await hub.pkgtools.ebuild.parallelize_pending_tasks()
 		hub.pop.sub.remove("my_catpkg")
 
-		for manifest_file, manifest_lines in hub.MANIFEST_LINES.items():
-			manifest_lines = sorted(list(manifest_lines))
-			with open(manifest_file, "w") as myf:
-				pos = 0
-				while pos < len(manifest_lines):
-					if pos != 0:
-						myf.write('\n')
-					myf.write(manifest_lines[pos])
-					pos += 1
-			logging.debug(f"Manifest {manifest_file} generated.")
+
+async def process_yaml_rule(hub, generator_sub, package=None, defaults=None, subpath=None, generator=None):
+	"""
+	This method takes a single YAML rule that we've extracted from an autogen.yaml file,
+	loads the appropriate generator, and uses it to generate (probably) a bunch of catpkgs.
+	"""
+
+	pkginfo = generator_sub.GLOBAL_DEFAULTS.copy()
+	pkginfo.update(defaults)
+	pkginfo['template_path'] = os.path.join(subpath, "templates")
+	if type(package) == str:
+		name = package
+	elif type(package) == dict:
+		# if any sub-arguments are specified with the package, we get it in this format:
+		# { 'pkgname' : { 'value1' : 'foo', 'value2' : 'bar' } }
+		name = list(package.keys())[0]
+		# get additional settings from YAML:
+		pkginfo.update(list(package.values())[0])
+	pkginfo['name'] = name
+	pkginfo['path'] = subpath
+	await generator_sub.generate(**pkginfo)
+
+
+async def generate_yaml_autogens(hub):
+	"""
+	This method finds autogen.yaml files in the repository and executes them. This provides a mechanism
+	to perform autogeneration en-masse without needing to have individual autogen.py files all over the
+	place.
+
+	Currently supported in the initial implementation are autogen.yaml files existing in *category*
+	directories.
+	"""
+	s, o = subprocess.getstatusoutput("find %s -iname autogen.yaml 2>&1" % hub.OPT.pkgtools['start_path'])
+	files = o.split('\n')
+	for file in files:
+		file = file.strip()
+		if not len(file):
+			continue
+		subpath = os.path.dirname(file)
+		with open(file, 'r') as myf:
+			for rule_name, rule in safe_load(myf.read()).items():
+				if 'defaults' in rule:
+					defaults = rule['defaults']
+				else:
+					defaults = {}
+				generator_sub = getattr(hub.pkgtools.generators, rule['generator'])
+				for package in rule['packages']:
+					await process_yaml_rule(hub, generator_sub, package=package, defaults=defaults, subpath=subpath)
+				await hub.pkgtools.ebuild.parallelize_pending_tasks()
+
+
+async def start(hub, start_path=None, out_path=None):
+
+	"""
+	This method will start the auto-generation of packages in an ebuild repository.
+	"""
+
+	hub.pkgtools.repository.set_context(
+		start_path if start_path is not None else hub.OPT.pkgtools.start_path,
+		out_path=out_path if out_path is not None else hub.OPT.pkgtools.out_path, name=hub.OPT.pkgtools.name)
+
+	await generate_individual_autogens(hub)
+	await generate_yaml_autogens(hub)
+	generate_manifests(hub)
+
 
 # vim: ts=4 sw=4 noet
