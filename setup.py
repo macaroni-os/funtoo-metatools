d35a4d975a0326ebf4c3acb3acf7f604fcfd28f9
Daniel Robbins <drobbins@funtoo.org>
2020-04-10 20:58:32 -0600

    Bumping to 0.4.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="funtoo-metatools",
-    version="0.4.1",
+    version="0.4.2",
     author="Daniel Robbins",
     author_email="drobbins@funtoo.org",
     description="Funtoo framework for auto-creation of ebuilds.",
