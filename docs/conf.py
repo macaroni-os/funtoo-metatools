a777d4ccb564636c23be1a34b0df07c260e6bc49
Daniel Robbins <drobbins@funtoo.org>
2020-08-09 19:38:34 -0600

    Use sphinx-rtd-theme. It's better.

--- docs/conf.py
+++ docs/conf.py
@@ -27,11 +27,6 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +42,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +49,12 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+
+import sphinx_rtd_theme
+
+extensions = [
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
