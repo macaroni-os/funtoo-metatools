dbd46706a47c1a6c12d5a3d2eb26471743cb5f2d
Daniel Robbins <drobbins@funtoo.org>
2020-09-23 19:47:04 -0600

    Workaround for some WIP stuff messing up tree gen.

--- funtoo/merge/deepdive.py
+++ funtoo/merge/deepdive.py
@@ -3,13 +3,13 @@ import pymongo
 from pymongo import MongoClient
 
 def __init__(hub):
-	dd = hub.DEEPDIVE = MongoClient().deepdive
-	dd.create_index("atom")
-	dd.create_index([("kit", pymongo.ASCENDING), ("category", pymongo.ASCENDING), ("package", pymongo.ASCENDING)])
-	dd.create_index("catpkg")
-	dd.create_index("relations")
-	dd.create_index("md5")
-
+	#dd = hub.DEEPDIVE = MongoClient().deepdive
+	#dd.create_index("atom")
+	#dd.create_index([("kit", pymongo.ASCENDING), ("category", pymongo.ASCENDING), ("package", pymongo.ASCENDING)])
+	#dd.create_index("catpkg")
+	#dd.create_index("relations")
+	#dd.create_index("md5")
+        pass
 
 async def populate_deepdive_database(hub):
 	hub.DEEPDIVE.delete_many({})
