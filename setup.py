3d4c00a1a197ba42d58ecb362f43de042f24c9d2
Daniel Robbins <drobbins@funtoo.org>
2020-11-20 10:03:06 -0700

    Remove httpx dep, add xmltodict because it is used in Funtoo by autogens.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
 		"Operating System :: POSIX :: Linux",
 	],
 	python_requires=">=3.7",
-	install_requires=["pop>=12", "Jinja2", "httpx", "aiodns", "aiohttp"],
+	install_requires=["pop>=12", "Jinja2", "xmltodict", "aiodns", "aiohttp"],
 	packages=setuptools.find_packages(),
 	package_data={"": ["*.tmpl"]},
 )
