a2d0f078d104ccd1b2bde12ea4722dc11b1c03a6
Daniel Robbins <drobbins@funtoo.org>
2022-08-19 14:32:22 -0600

    1.1.0 release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
 	name="funtoo-metatools",
-	version="1.0.5",
+	version="1.1.0",
 	author="Daniel Robbins",
 	author_email="drobbins@funtoo.org",
 	description="Funtoo framework for auto-creation of ebuilds.",
